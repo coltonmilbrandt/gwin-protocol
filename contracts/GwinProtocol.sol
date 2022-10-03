b307e0c6fd141cd4b0139e62649fa9f14171decd
coltonmilbrandt <coltonmilbrandt@gmail.com>
2022-10-03 10:35:00 -0600

    added tranch allocation math

--- contracts/GwinProtocol.sol
+++ contracts/GwinProtocol.sol
@@ -16,9 +16,12 @@ contract GwinProtocol is Ownable {
     // address[] public stakers;
     // // array of the allowed tokens
     // address[] public allowedTokens;
-    uint256 lastEthUsd;
-    uint256 currentEthUsd;
+    uint256 lastSettledEthUsd;
     uint32 bp = 10000;
+    // TEMP simulated balance
+    uint256 cEthBal = 10;
+    // TEMP simulated balance
+    uint256 hEthBal = 10;
 
     // Storing the GWIN token as a global variable, IERC20 imported above, address passed into constructor
     IERC20 public gwinToken;
@@ -38,13 +41,72 @@ contract GwinProtocol is Ownable {
         currentEthUsd = 1100;
     }
 
-    function getProfit() public view returns (uint256) {
-        uint256 profit = ((currentEthUsd - lastEthUsd) * bps) / lastEthUsd;
+    function getProfit(uint256 _ethUSd) public view returns (uint256) {
+        uint256 profit = ((_ethUSd - lastSettledEthUsd) * bps) /
+            lastSettledEthUsd;
         return profit;
     }
 
+    // TEMP returns current ETH/USD
+    function getCurrentEthUsd() public returns (uint256) {
+        return 1100;
+    }
+
+    // make modular function that does calculations that both tranches need
+    function trancheSpecificCalcs(
+        bool _isCooled,
+        uint256 _ethUsdProfit,
+        uint256 _currentEthUsd
+    ) public returns (uint256) {
+        if (_isCooled == true) {
+            uint256 trancheBal = cEthBal;
+            uint256 r = -0.5;
+        } else {
+            uint256 trancheBal = hEthBal;
+            uint256 r = 0.5;
+        }
+        uint256 trancheChange = (trancheBal * _currentEthUsd) -
+            (trancheBal * lastSettledEthUsd);
+        uint256 expectedPayout = trancheChange * (1 + r);
+        uint256 allocationDifference = expectedPayout - trancheChange;
+        return allocationDifference;
+    }
+
     // Interact: cEthBal, hEthBal, cEthUsdBal, hEthUsdBal, tranche, lastEthUsd, currentEthUsd
-    function interact(uint256 _num) public {}
+    function interact(bool _isCooled) public {
+        uint256 currentEthUsd = getCurrentEthUsd();
+        uint256 ethUsdProfit = getProfit(currentEthUsd);
+        uint256 cooledAllocationDiff = trancheSpecificCalcs(
+            true,
+            ethUsdProfit,
+            currentEthUsd
+        );
+        uint256 heatedAllocationDiff = trancheSpecificCalcs(
+            false,
+            ethUsdProfit,
+            currentEthUsd
+        );
+        // calculate out min
+        // both tranche are needed for the diff
+        uint256 absHeatedAllocationDiff = abs(heatedAllocationDiff);
+        uint256 absCooledAllocationDiff = abs(cooledAllocationDiff);
+        uint256 minAbsAllocation = absCooledAllocationDiff >
+            absHeatedAllocationDiff
+            ? absHeatedAllocationDiff
+            : absCooledAllocationDiff;
+        uint256 nonNaturalDifference = absHeatedAllocationDiff +
+            absCooledAllocationDiff;
+        uint256 percentCooledTranche = (cEthBal * bps) / (cEthBal + hEthBal);
+        uint256 nonNaturalMultiplier = ethUsdProfit > 0
+            ? percentCooledTranche
+            : (1 - percentCooledTranche);
+        uint256 adjNonNaturalDiff = abs(nonNaturalDifference) *
+            nonNaturalMultiplier;
+    }
+
+    function abs(uint x) private pure returns (int) {
+        return x >= 0 ? x : -x;
+    }
 
     // Pass in: address, tranche, amount, hotColdRatio, priceChange
 }
